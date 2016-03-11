59b5d6f25376042df805c9d1ed0d760c1e96754a
xlm <1158529273@qq.com>
2016-03-11 20:54:50 +0800

    v2.2

--- rango/forms.py
+++ rango/forms.py
@@ -15,11 +15,21 @@ class CategoryForm( forms. ModelForm) :
 		fields = ( 'name' , )
 class PageForm( forms.ModelForm) :
 	page_id = forms.IntegerField(help_text="Please enter the id of the page. ")
-	title = forms.CharField( max_length=128, help_text="Please enter the title of the page. ")
-	url = forms.URLField( max_length=200, help_text="Please enter the URL of the page. ")
-	body = forms.CharField(max_length=128,help_text="Please enter the body of the page. ")
-    	answer = forms.CharField(max_length=128,help_text="Please enter the answer of the page. ")
-	views = forms.IntegerField( widget=forms.HiddenInput( ) , initial=0)
+	stage = forms.CharField( max_length=128, help_text="请输入所属阶段（选项）")
+	mytype = forms.CharField( max_length=128, help_text="请输入#题目类型（选项单选、多选、判断、填空、简答、代码、实操）")
+	key = forms.CharField( max_length=128, help_text="请输入关键字")
+	level = forms.CharField( max_length=128, help_text="请输入难度等级（选项ABCD） ")
+	body = forms.CharField( max_length=128, help_text="请输入题干")
+	option_A = forms.CharField( max_length=128, help_text="请输入A 选项 ")
+	option_B = forms.CharField( max_length=128, help_text="请输入B 选项")
+	option_C = forms.CharField( max_length=128, help_text="请输入C 选项")
+	option_D = forms.CharField( max_length=128, help_text="请输入D 选项")
+	option_E = forms.CharField( max_length=128, help_text="请输入E 选项 ")
+	option_F = forms.CharField( max_length=128, help_text="请输入F 选项")
+	information = forms.CharField( max_length=128, help_text="请输入所属知识点（读关联  分隔;）")	
+    	answer = forms.CharField(max_length=128,help_text="请输入答案 ")
+	explain = forms.CharField(max_length=128,help_text="请输入答案说明")
+	author = forms.CharField(max_length=128,help_text="请输入出题人")
 	class Meta:
 # model建立表单和模型类的关联， 排除exclude里包含的字段， 其它字段显示
 		model = Page
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
@@ -16,15 +17,25 @@ class Category(models.Model) :
     def __unicode__(self) :
         return self.name
 class Page(models.Model) :
-    category = models.ForeignKey(Category)
-    page_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=128)
-    body = models.CharField(max_length=128, default=0)
-    answer = models.CharField(max_length=128, default=0)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category)#所属科目（选项）
+    page_id = models.IntegerField(unique=True,default=0)#题目的唯一ID
+    stage = models.CharField(max_length=128)#所属阶段（选项）
+    mytype = models.CharField(max_length=28, default=0)#题目类型（选项单选、多选、判断、填空、简答、代码、实操）
+    key = models.CharField(max_length=28, default=0)#关键字
+    level = models.CharField(max_length=28, default=0)#难度等级（选项ABCD）
+    body = models.CharField(max_length=128, default=0)#题干
+    option_A = models.CharField(max_length=128, default=0)#A 选项
+    option_B = models.CharField(max_length=128, default=0)#B 选项
+    option_C = models.CharField(max_length=128, default=0)#C 选项
+    option_D = models.CharField(max_length=128, default=0)#D 选项
+    option_E = models.CharField(max_length=128, default=0)#E 选项
+    option_F = models.CharField(max_length=128, default=0)#F 选项  
+    information = models.CharField(max_length=28, default=0)#所属知识点（读关联  分隔;）
+    answer = models.CharField(max_length=128, default=0)#答案
+    explain = models.CharField(max_length=1028, default=0)#答案说明
+    author = models.CharField(max_length=1028, default=0)#出题人
     def __unicode__(self) : #Python2, use __str__ on Python3
-        return self.title
+        return self.page_id
 
 class UserProfile(models.Model) :
     user = models.OneToOneField(User)
